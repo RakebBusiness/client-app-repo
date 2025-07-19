75f624ea233013d375a83410eac4187601c308cc
RakebBusiness <rakebbusiness@gmail.com>
2025-07-19 01:59:36 +0100

    Switch from Google Maps to OpenStreetMap with Flutter Map

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import '../profile/profile_screen.dart';
 import '../trips/trips_screen.dart';
 import '../promotions/promotions_screen.dart';
@@ -12,17 +13,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late GoogleMapController mapController;
+  final MapController mapController = MapController();
   
   // Default location (Algiers, Algeria)
-  static const CameraPosition _initialPosition = CameraPosition(
-    target: LatLng(36.7538, 3.0588),
-    zoom: 14.0,
-  );
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
+  static const LatLng _initialPosition = LatLng(36.7538, 3.0588);
 
   void _showNavigationMenu() {
     showModalBottomSheet(
@@ -90,14 +84,43 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          // Google Maps
-          GoogleMap(
-            onMapCreated: _onMapCreated,
-            initialCameraPosition: _initialPosition,
-            myLocationEnabled: true,
-            myLocationButtonEnabled: false,
-            zoomControlsEnabled: false,
-            mapToolbarEnabled: false,
+          // OpenStreetMap with Flutter Map
+          FlutterMap(
+            mapController: mapController,
+            options: MapOptions(
+              initialCenter: _initialPosition,
+              initialZoom: 14.0,
+              minZoom: 3.0,
+              maxZoom: 18.0,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.flutter_application_1',
+                maxZoom: 18,
+              ),
+              MarkerLayer(
+                markers: [
+                  Marker(
+                    point: _initialPosition,
+                    width: 40,
+                    height: 40,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF32C156),
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Colors.white, width: 3),
+                      ),
+                      child: const Icon(
+                        Icons.person,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
           
           // Top overlay with menu button
@@ -151,6 +174,87 @@ class _HomeScreenState extends State<HomeScreen> {
                     color: Color(0xFF32C156),
                   ),
                 ),
+              ),
+            ),
+          ),
+
+          // Zoom controls
+          Positioned(
+            right: 16,
+            top: MediaQuery.of(context).padding.top + 80,
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 4,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: IconButton(
+                    icon: const Icon(Icons.add, color: Color(0xFF32C156)),
+                    onPressed: () {
+                      mapController.move(
+                        mapController.camera.center,
+                        mapController.camera.zoom + 1,
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 4,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: IconButton(
+                    icon: const Icon(Icons.remove, color: Color(0xFF32C156)),
+                    onPressed: () {
+                      mapController.move(
+                        mapController.camera.center,
+                        mapController.camera.zoom - 1,
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // My location button
+          Positioned(
+            right: 16,
+            bottom: 120,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 8,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.my_location, color: Color(0xFF32C156)),
+                onPressed: () {
+                  mapController.move(_initialPosition, 14.0);
+                },
+                iconSize: 24,
               ),
             ),
           ),
