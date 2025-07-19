111fe6e856e723d0ea7b7b53b4136d25bdb9f140
RakebBusiness <rakebbusiness@gmail.com>
2025-07-19 02:44:27 +0100

    Fix maps not showing on mobile devices

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
 import '../profile/profile_screen.dart';
 import '../trips/trips_screen.dart';
 import '../promotions/promotions_screen.dart';
@@ -17,6 +18,32 @@ class _HomeScreenState extends State<HomeScreen> {
   
   // Default location (Algiers, Algeria)
   static const LatLng _initialPosition = LatLng(36.7538, 3.0588);
+  bool _mapLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkMapTiles();
+  }
+
+  Future<void> _checkMapTiles() async {
+    try {
+      final response = await http.get(
+        Uri.parse('https://tile.openstreetmap.org/14/8239/6057.png'),
+      );
+      if (response.statusCode == 200) {
+        setState(() {
+          _mapLoaded = true;
+        });
+      }
+    } catch (e) {
+      print('Map tiles check failed: $e');
+      // Still show map even if check fails
+      setState(() {
+        _mapLoaded = true;
+      });
+    }
+  }
 
   void _showNavigationMenu() {
     showModalBottomSheet(
@@ -85,43 +112,69 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Stack(
         children: [
           // OpenStreetMap with Flutter Map
-          FlutterMap(
-            mapController: mapController,
-            options: MapOptions(
-              initialCenter: _initialPosition,
-              initialZoom: 14.0,
-              minZoom: 3.0,
-              maxZoom: 18.0,
-            ),
-            children: [
-              TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                userAgentPackageName: 'com.example.flutter_application_1',
-                maxZoom: 18,
-              ),
-              MarkerLayer(
-                markers: [
-                  Marker(
-                    point: _initialPosition,
-                    width: 40,
-                    height: 40,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF32C156),
-                        borderRadius: BorderRadius.circular(20),
-                        border: Border.all(color: Colors.white, width: 3),
-                      ),
-                      child: const Icon(
-                        Icons.person,
-                        color: Colors.white,
-                        size: 20,
-                      ),
+          _mapLoaded
+              ? FlutterMap(
+                  mapController: mapController,
+                  options: MapOptions(
+                    initialCenter: _initialPosition,
+                    initialZoom: 14.0,
+                    minZoom: 3.0,
+                    maxZoom: 18.0,
+                  ),
+                  children: [
+                    TileLayer(
+                      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                      userAgentPackageName: 'com.rakib.app',
+                      maxZoom: 18,
+                      additionalOptions: const {
+                        'id': 'openstreetmap',
+                      },
+                      tileProvider: NetworkTileProvider(),
+                    ),
+                    MarkerLayer(
+                      markers: [
+                        Marker(
+                          point: _initialPosition,
+                          width: 40,
+                          height: 40,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xFF32C156),
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(color: Colors.white, width: 3),
+                            ),
+                            child: const Icon(
+                              Icons.person,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              : Container(
+                  color: Colors.grey[200],
+                  child: const Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircularProgressIndicator(
+                          color: Color(0xFF32C156),
+                        ),
+                        SizedBox(height: 16),
+                        Text(
+                          'Loading map...',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
               ),
-            ],
-          ),
           
           // Top overlay with menu button
           Positioned(
